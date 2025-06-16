c8a9e47d4ef67f6f79ee1839c0168a3e454cecdd
LukaLominadze <fguru74@gmail.ccom>
2025-06-16 23:54:07 +0400

    refactoring

--- Kakali/src/gl/Framebuffer.cpp
+++ Kakali/src/gl/Framebuffer.cpp
@@ -1,6 +1,8 @@
 #include "Framebuffer.h"
 #include <iostream>
 
+#include "core/Log.h"
+
 FrameBuffer::FrameBuffer()
 	:m_rendererID(0), m_colorAttachment(0), m_depthAttachment(0)
 {
@@ -8,56 +10,56 @@ FrameBuffer::FrameBuffer()
 
 void FrameBuffer::GenBuffer(const FrameBufferSpecification& props)
 {
-	glCreateFramebuffers(1, &m_rendererID);
-	glBindFramebuffer(GL_FRAMEBUFFER, m_rendererID);
+	GLCall(glCreateFramebuffers(1, &m_rendererID));
+	GLCall(glBindFramebuffer(GL_FRAMEBUFFER, m_rendererID));
 
 	if ((m_spec.Attachments & FrameBufferAttachments::COLOR) == FrameBufferAttachments::COLOR) {
-		glCreateTextures(GL_TEXTURE_2D, 1, &m_colorAttachment);
-		glBindTexture(GL_TEXTURE_2D, m_colorAttachment);
-		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, m_spec.Width, m_spec.Height, 0, GL_RGBA, GL_UNSIGNED_BYTE, nullptr);
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+		GLCall(glCreateTextures(GL_TEXTURE_2D, 1, &m_colorAttachment));
+		GLCall(glBindTexture(GL_TEXTURE_2D, m_colorAttachment));
+		GLCall(glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, m_spec.Width, m_spec.Height, 0, GL_RGBA, GL_UNSIGNED_BYTE, nullptr));
+		GLCall(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST));
+		GLCall(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST));
 
-		glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, m_colorAttachment, 0);
+		GLCall(glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, m_colorAttachment, 0));
 	}
 	if ((m_spec.Attachments & FrameBufferAttachments::DEPTH) == FrameBufferAttachments::DEPTH) {
-		glCreateTextures(GL_TEXTURE_2D, 1, &m_depthAttachment);
-		glBindTexture(GL_TEXTURE_2D, m_depthAttachment);
+		GLCall(glCreateTextures(GL_TEXTURE_2D, 1, &m_depthAttachment));
+		GLCall(glBindTexture(GL_TEXTURE_2D, m_depthAttachment));
 
-		glTexImage2D(GL_TEXTURE_2D, 0, GL_DEPTH_COMPONENT, m_spec.Width, m_spec.Height, 0, GL_DEPTH_COMPONENT, GL_FLOAT, NULL);
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+		GLCall(glTexImage2D(GL_TEXTURE_2D, 0, GL_DEPTH_COMPONENT, m_spec.Width, m_spec.Height, 0, GL_DEPTH_COMPONENT, GL_FLOAT, NULL));
+		GLCall(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST));
+		GLCall(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST));
+		GLCall(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE));
+		GLCall(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE));
 
-		glFramebufferTexture2D(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_TEXTURE_2D, m_depthAttachment, 0);
+		GLCall(glFramebufferTexture2D(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_TEXTURE_2D, m_depthAttachment, 0));
 	}
 	if ((m_spec.Attachments & FrameBufferAttachments::DEPTH_STENCIL) == FrameBufferAttachments::DEPTH_STENCIL) {
-		glCreateTextures(GL_TEXTURE_2D, 1, &m_depthAttachment);
-		glBindTexture(GL_TEXTURE_2D, m_depthAttachment);
-		glTexImage2D(GL_TEXTURE_2D, 0, GL_DEPTH24_STENCIL8, m_spec.Width, m_spec.Height, 0, GL_DEPTH_STENCIL,
-			GL_UNSIGNED_INT_24_8, NULL);
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_BORDER);
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_BORDER);
+		GLCall(glCreateTextures(GL_TEXTURE_2D, 1, &m_depthAttachment));
+		GLCall(glBindTexture(GL_TEXTURE_2D, m_depthAttachment));
+		GLCall(glTexImage2D(GL_TEXTURE_2D, 0, GL_DEPTH24_STENCIL8, m_spec.Width, m_spec.Height, 0, GL_DEPTH_STENCIL,
+			GL_UNSIGNED_INT_24_8, NULL));
+		GLCall(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_BORDER));
+		GLCall(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_BORDER));
 
-		glFramebufferTexture2D(GL_FRAMEBUFFER, GL_DEPTH_STENCIL_ATTACHMENT, GL_TEXTURE_2D, m_depthAttachment, 0);
+		GLCall(glFramebufferTexture2D(GL_FRAMEBUFFER, GL_DEPTH_STENCIL_ATTACHMENT, GL_TEXTURE_2D, m_depthAttachment, 0));
 	}
 
 	if (glCheckFramebufferStatus(GL_FRAMEBUFFER) == GL_FRAMEBUFFER_COMPLETE) {
 		std::cout << "Framebuffer complete!" << std::endl;
 	}
 
-	glBindFramebuffer(GL_FRAMEBUFFER, 0);
+	GLCall(glBindFramebuffer(GL_FRAMEBUFFER, 0));
 }
 
 void FrameBuffer::Delete()
 {
 	if ((m_spec.Attachments & FrameBufferAttachments::COLOR) == FrameBufferAttachments::COLOR)
-		glDeleteTextures(1, &m_colorAttachment);
+		GLCall(glDeleteTextures(1, &m_colorAttachment));
 	if ((m_spec.Attachments & FrameBufferAttachments::DEPTH) == FrameBufferAttachments::DEPTH ||
 		(m_spec.Attachments & FrameBufferAttachments::DEPTH_STENCIL) == FrameBufferAttachments::DEPTH_STENCIL)
-		glDeleteTextures(1, &m_depthAttachment);
-	glDeleteFramebuffers(1, &m_rendererID);
+		GLCall(glDeleteTextures(1, &m_depthAttachment));
+	GLCall(glDeleteFramebuffers(1, &m_rendererID));
 }
 
 void FrameBuffer::Resize(uint32_t width, uint32_t height)
@@ -72,61 +74,61 @@ void FrameBuffer::Invalidate()
 {
 	if (m_rendererID) {
 		if ((m_spec.Attachments & FrameBufferAttachments::COLOR) == FrameBufferAttachments::COLOR)
-			glDeleteTextures(1, &m_colorAttachment);
+			GLCall(glDeleteTextures(1, &m_colorAttachment));
 		if ((m_spec.Attachments & FrameBufferAttachments::DEPTH) == FrameBufferAttachments::DEPTH ||
 			(m_spec.Attachments & FrameBufferAttachments::DEPTH_STENCIL) == FrameBufferAttachments::DEPTH_STENCIL)
-			glDeleteTextures(1, &m_depthAttachment);
-		glDeleteFramebuffers(1, &m_rendererID);
+			GLCall(glDeleteTextures(1, &m_depthAttachment));
+		GLCall(glDeleteFramebuffers(1, &m_rendererID));
 	}
 
-	glCreateFramebuffers(1, &m_rendererID);
-	glBindFramebuffer(GL_FRAMEBUFFER, m_rendererID);
+	GLCall(glCreateFramebuffers(1, &m_rendererID));
+	GLCall(glBindFramebuffer(GL_FRAMEBUFFER, m_rendererID));
 
 	if ((m_spec.Attachments & FrameBufferAttachments::COLOR) == FrameBufferAttachments::COLOR) {
-		glCreateTextures(GL_TEXTURE_2D, 1, &m_colorAttachment);
-		glBindTexture(GL_TEXTURE_2D, m_colorAttachment);
-		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, m_spec.Width, m_spec.Height, 0, GL_RGBA, GL_UNSIGNED_BYTE, nullptr);
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+		GLCall(glCreateTextures(GL_TEXTURE_2D, 1, &m_colorAttachment));
+		GLCall(glBindTexture(GL_TEXTURE_2D, m_colorAttachment));
+		GLCall(glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, m_spec.Width, m_spec.Height, 0, GL_RGBA, GL_UNSIGNED_BYTE, nullptr));
+		GLCall(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST));
+		GLCall(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST));
 
-		glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, m_colorAttachment, 0);
+		GLCall(glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, m_colorAttachment, 0));
 	}
 	if ((m_spec.Attachments & FrameBufferAttachments::DEPTH) == FrameBufferAttachments::DEPTH) {
-		glCreateTextures(GL_TEXTURE_2D, 1, &m_depthAttachment);
-		glBindTexture(GL_TEXTURE_2D, m_depthAttachment);
+		GLCall(glCreateTextures(GL_TEXTURE_2D, 1, &m_depthAttachment));
+		GLCall(glBindTexture(GL_TEXTURE_2D, m_depthAttachment));
 
-		glTexImage2D(GL_TEXTURE_2D, 0, GL_DEPTH_COMPONENT, m_spec.Width, m_spec.Height, 0, GL_DEPTH_COMPONENT, GL_FLOAT, NULL);
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+		GLCall(glTexImage2D(GL_TEXTURE_2D, 0, GL_DEPTH_COMPONENT, m_spec.Width, m_spec.Height, 0, GL_DEPTH_COMPONENT, GL_FLOAT, NULL));
+		GLCall(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST));
+		GLCall(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST));
+		GLCall(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE));
+		GLCall(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE));
 
-		glFramebufferTexture2D(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_TEXTURE_2D, m_depthAttachment, 0);
+		GLCall(glFramebufferTexture2D(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_TEXTURE_2D, m_depthAttachment, 0));
 	}
 	if ((m_spec.Attachments & FrameBufferAttachments::DEPTH_STENCIL) == FrameBufferAttachments::DEPTH_STENCIL) {
-		glCreateTextures(GL_TEXTURE_2D, 1, &m_depthAttachment);
-		glBindTexture(GL_TEXTURE_2D, m_depthAttachment);
-		glTexImage2D(GL_TEXTURE_2D, 0, GL_DEPTH24_STENCIL8, m_spec.Width, m_spec.Height, 0, GL_DEPTH_STENCIL,
-			GL_UNSIGNED_INT_24_8, NULL);
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_BORDER);
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_BORDER);
+		GLCall(glCreateTextures(GL_TEXTURE_2D, 1, &m_depthAttachment));
+		GLCall(glBindTexture(GL_TEXTURE_2D, m_depthAttachment));
+		GLCall(glTexImage2D(GL_TEXTURE_2D, 0, GL_DEPTH24_STENCIL8, m_spec.Width, m_spec.Height, 0, GL_DEPTH_STENCIL,
+			GL_UNSIGNED_INT_24_8, NULL));
+		GLCall(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_BORDER));
+		GLCall(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_BORDER));
 
-		glFramebufferTexture2D(GL_FRAMEBUFFER, GL_DEPTH_STENCIL_ATTACHMENT, GL_TEXTURE_2D, m_depthAttachment, 0);
+		GLCall(glFramebufferTexture2D(GL_FRAMEBUFFER, GL_DEPTH_STENCIL_ATTACHMENT, GL_TEXTURE_2D, m_depthAttachment, 0));
 	}
 
 	if (glCheckFramebufferStatus(GL_FRAMEBUFFER) != GL_FRAMEBUFFER_COMPLETE) {
 		std::cout << "Framebuffer not complete!" << std::endl;
 	}
 
-	glBindFramebuffer(GL_FRAMEBUFFER, 0);
+	GLCall(glBindFramebuffer(GL_FRAMEBUFFER, 0));
 }
 
 void FrameBuffer::Bind() const
 {
-	glBindFramebuffer(GL_FRAMEBUFFER, m_rendererID);
+	GLCall(glBindFramebuffer(GL_FRAMEBUFFER, m_rendererID));
 }
 
 void FrameBuffer::Unbind() const
 {
-	glBindFramebuffer(GL_FRAMEBUFFER, 0);
+	GLCall(glBindFramebuffer(GL_FRAMEBUFFER, 0));
 }
--- Kakali/src/gl/Shader.cpp
+++ Kakali/src/gl/Shader.cpp
@@ -34,86 +34,86 @@ void Shader::LoadShader(const char* vertexPath, const char* fragmentPath)
     const char* fShaderCode = fragmentCode.c_str();
 
     unsigned int vertexShader;
-    vertexShader = glCreateShader(GL_VERTEX_SHADER);
-    glShaderSource(vertexShader, 1, &vShaderCode, NULL);
-    glCompileShader(vertexShader);
+    GLCall(vertexShader = glCreateShader(GL_VERTEX_SHADER));
+    GLCall(glShaderSource(vertexShader, 1, &vShaderCode, NULL));
+    GLCall(glCompileShader(vertexShader));
 
     int  success;
     char infoLog[512];
-    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+    GLCall(glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success));
 
     if (!success)
     {
-        glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+        GLCall(glGetShaderInfoLog(vertexShader, 512, NULL, infoLog));
         std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << vShaderCode << infoLog << std::endl;
     }
 
     unsigned int fragmentShader;
-    fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-    glShaderSource(fragmentShader, 1, &fShaderCode, NULL);
-    glCompileShader(fragmentShader);
+    GLCall(fragmentShader = glCreateShader(GL_FRAGMENT_SHADER));
+    GLCall(glShaderSource(fragmentShader, 1, &fShaderCode, NULL));
+    GLCall(glCompileShader(fragmentShader));
 
-    glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+    GLCall(glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success));
 
     if (!success)
     {
-        glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+        GLCall(glGetShaderInfoLog(vertexShader, 512, NULL, infoLog));
         std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << fShaderCode << infoLog << std::endl;
     }
 
     /* Create and link program */
-    m_rendererID = glCreateProgram();
+    GLCall(m_rendererID = glCreateProgram());
 
-    glAttachShader(m_rendererID, vertexShader);
-    glAttachShader(m_rendererID, fragmentShader);
-    glLinkProgram(m_rendererID);
+    GLCall(glAttachShader(m_rendererID, vertexShader));
+    GLCall(glAttachShader(m_rendererID, fragmentShader));
+    GLCall(glLinkProgram(m_rendererID));
 
-    glGetProgramiv(m_rendererID, GL_LINK_STATUS, &success);
+    GLCall(glGetProgramiv(m_rendererID, GL_LINK_STATUS, &success));
     if (!success) {
-        glGetProgramInfoLog(m_rendererID, 512, NULL, infoLog);
+        GLCall(glGetProgramInfoLog(m_rendererID, 512, NULL, infoLog));
         std::cout << "ERROR::SHADER::PROGRAM::COMPILATION_FAILED\n" << infoLog << std::endl;
     }
 
-    glDeleteShader(vertexShader);
-    glDeleteShader(fragmentShader);
+    GLCall(glDeleteShader(vertexShader));
+    GLCall(glDeleteShader(fragmentShader));
 }
 
 void Shader::Delete()
 {
-    glDeleteProgram(m_rendererID);
+    GLCall(glDeleteProgram(m_rendererID));
 }
 
 void Shader::Use()
 {
-    glUseProgram(m_rendererID);
+    GLCall(glUseProgram(m_rendererID));
 }
 
 void Shader::SetBool(const std::string& name, bool value) const
 {
-    glUniform1i(glGetUniformLocation(m_rendererID, name.c_str()), (int)value);
+    GLCall(glUniform1i(glGetUniformLocation(m_rendererID, name.c_str()), (int)value));
 }
 
 void Shader::SetInt(const std::string& name, int value) const
 {
-    glUniform1i(glGetUniformLocation(m_rendererID, name.c_str()), value);
+    GLCall(glUniform1i(glGetUniformLocation(m_rendererID, name.c_str()), value));
 }
 
 void Shader::SetFloat(const std::string& name, float value) const
 {
-    glUniform1f(glGetUniformLocation(m_rendererID, name.c_str()), value);
+    GLCall(glUniform1f(glGetUniformLocation(m_rendererID, name.c_str()), value));
 }
 
 void Shader::SetFloat3(const std::string& name, float v0, float v1, float v2) const
 {
-    glUniform3f(glGetUniformLocation(m_rendererID, name.c_str()), v0, v1, v2);
+    GLCall(glUniform3f(glGetUniformLocation(m_rendererID, name.c_str()), v0, v1, v2));
 }
 
 void Shader::SetIntV(const std::string& name, unsigned int count, int* value)
 {
-    glUniform1iv(glGetUniformLocation(m_rendererID, name.c_str()), count, value);
+    GLCall(glUniform1iv(glGetUniformLocation(m_rendererID, name.c_str()), count, value));
 }
 
 void Shader::SetMat4f(const std::string& name, const glm::mat4& matrix)
 {
-    glUniformMatrix4fv(glGetUniformLocation(m_rendererID, name.c_str()), 1, GL_FALSE, &matrix[0][0]);
+    GLCall(glUniformMatrix4fv(glGetUniformLocation(m_rendererID, name.c_str()), 1, GL_FALSE, &matrix[0][0]));
 }
